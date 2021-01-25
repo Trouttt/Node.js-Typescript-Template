9317ab4e527c781b353f24d014e26c406aaf3f66
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-01-25 17:06:18 -0300

    featured: adding debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request,response) => {
+app.use(express.json());
+
+app.get("/user", (request,response) => {
+    const { name, email } = request.body
+
+    const user = {
+        name,
+        email
+    };
+    
     return response.json({ message: 'Hello world'});
+
 })
 
 app.listen(3333, () => {
