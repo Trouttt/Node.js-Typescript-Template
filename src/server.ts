24b9ee0a3c82d052bbdaaccfad5e7ab19fa94702
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-03-22 20:31:54 -0300

    Added: editorconfig, eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,7 @@
-import express from 'express';
+/* eslint-disable no-console */
+import express from "express";
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/user", (request,response) => {
-    const { name, email } = request.body
-
-    const user = {
-        name,
-        email
-    };
-    
-    return response.json({ message: 'Hello world'});
-
-})
-
-app.listen(3333, () => {
-    console.log("Server started on port 3333");
-})
+app.get("/", (request, response) => response.json({ message: "heloworld" }));
+app.listen(3333, () => console.log("oi"));
